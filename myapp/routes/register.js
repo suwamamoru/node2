3f59190c953bce52dd8b32d14b0122d499cd8295
諏訪守 <suwamamoru@suwamamorunoMacBook-Air.local>
2022-03-01 11:59:27 +0900

    fix1

--- myapp/routes/register.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const registerController = require('../controllers/registerController');
-const { registerValidator } = require('../models/validator');
-
-router.post('/dashboard', registerValidator(), registerController.register);
-
-module.exports = router;
